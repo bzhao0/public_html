75424535a8504b40cf7858970009ec8b1ff55702
bzhao0 <brandenzhao125@gmail.com>
2025-07-24 15:21:09 -0400

    finished word fold and started goat game

--- word_fold/rbow.js
+++ word_fold/rbow.js
@@ -2,7 +2,7 @@ let angle3 = 0.0;
 
 function onframe(){
     document.documentElement.style.setProperty('--hue-angle', angle3 + 'deg');
-    angle3 += 10;
+    angle3 += 8;
 
     requestAnimationFrame(onframe);
 }
