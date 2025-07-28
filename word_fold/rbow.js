1e6b19fa37fe21e508b3107d525bd5341ff5e671
bzhao0 <brandenzhao125@gmail.com>
2025-07-28 16:05:29 -0400

    huigiengejodhbujd

--- word_fold/rbow.js
+++ word_fold/rbow.js
@@ -1,7 +1,7 @@
 let angle3 = 0.0;
 
 function onframe(){
-    document.documentElement.style.setProperty('--hue-angle', angle3 + 'deg');
+    document.documentElement.style.setProperty('', angle3 + 'deg');
     angle3 += 8;
 
     requestAnimationFrame(onframe);
